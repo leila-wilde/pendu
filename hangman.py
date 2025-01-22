cff8dd9a7862d507ea0637e6b01d919a9e0bf024
morgane-rossi <morgane_rt@hotmail.com>
2025-01-22 18:23:27 +0100

    added some comments

--- hangman.py
+++ hangman.py
@@ -2,6 +2,10 @@ import words
 from random import randrange
 
 def initiate():
+    """
+    Selects randomly and returns a word
+    within variable list_of_words inside words.py file
+    """
     print("Bienvenue au jeu du pendu !")
     list_words = words.list_of_words
     nbWords = len(list_words)
@@ -10,10 +14,13 @@ def initiate():
 
 
 def guessing(play_word):
+    # we keep record of letters already played so we display this in order to help players
+    # list of letters used by player. Initially empty
     list_of_letters = []
     len_word = len(play_word)
 
-    # we keep record of letters already played so we display this in order to help players
+    # we use a list to store letters that are part of the word to guess
+    # initially, this is a list of "_" character
     tab_play_word = ["_"] * len_word
     print("".join(tab_play_word))
 
@@ -22,7 +29,10 @@ def guessing(play_word):
     counter = 0
     finished = False
 
-    while not finished and counter < 8 :
+    # games stops either after 7 errors, either when correct word is guessed
+    while not finished and counter < 7 :
+
+
         erreur = True
 
         print("Lettres déjà jouées :")
@@ -30,11 +40,17 @@ def guessing(play_word):
         letter = input_letter()
         for id_letter in range(len_word) :
             if play_word[id_letter] == letter.lower() :
+
+                # boolean value. there is an error if letter is not part of the word to guess
                 erreur = False
+
+                # uppercase & lowercase are handled equally
                 if tab_play_word[id_letter] == "_" :
                     tab_play_word[id_letter] = letter.lower()
         if letter not in list_of_letters :
             list_of_letters.append(letter)
+
+            # error counter is incremented only if a bad letter hasn't been used already
             if erreur :
                 counter += 1
         print(f"compteur : {counter}")
@@ -49,7 +65,9 @@ def guessing(play_word):
         print(f"Le mot à trouver était : {play_word}")
 
 def input_letter():
-
+    """
+    User cannot enter an input different from a single letter.
+    """
     letter = ""
     while len(letter) != 1 or not letter.isalpha() :
         try :
